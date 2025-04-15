6cfe3809f0e7a6fcdeed5f22e8350e99d910c13c
Marco Di Toro <42249455+Frombull@users.noreply.github.com>
2025-04-15 15:18:33 -0300

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from "@/components/login-form"
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gradient-to-b from-blue-50 to-blue-100 flex items-center justify-center p-4 md:p-8">
-      <LoginForm />
-    </main>
-  )
-}
